7bfec9fb52cf64b7b5d7ea6d0a8f7ccaa6cf0503
Felipe Santos <felipefss@gmail.com>
2021-11-19 11:25:04 +0000

    Add a nav.

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,22 @@
+import { Container } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
 function HomePage() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <Navbar bg="primary" variant="dark" expand="sm">
+        <Container fluid>
+          <Navbar.Brand href="#">MyDo List</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav.Link className="nav-text" bsPrefix="" href="#">New List</Nav.Link>
+            <Nav.Link className="nav-text" href="#">Settings</Nav.Link>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default HomePage;
